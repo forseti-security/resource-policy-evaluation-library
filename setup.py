253aad005760a7efae016d2ea7b81807c933fb71
Joe Ceresini <joe.ceresini@cleardata.com>
2019-01-28 17:16:09 -0500

    Use scm for versionining in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from __future__ import print_function
 
 from setuptools import setup
-from micromanager import __version__ as version
 
 setup(
     name="micromanager",
-    version=version,
     description="Policy enforcement tool for GCP",
     long_description='Tool to load resources from GCP, check them against policies written in Rego, and attempt remediation of policy violations.',
     author="Joe Ceresini",
     url="https://github.com/cleardataeng/micromanager",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=[
         'google-api-python-client',
         'google-api-python-client-helpers',
