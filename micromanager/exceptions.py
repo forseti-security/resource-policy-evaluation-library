430f1747003486d1057306a233f6a2c98ecbae7e
Joe Ceresini <joe.ceresini@cleardata.com>
2019-03-01 13:56:20 -0500

    Default to False for is_retryable_exception

--- micromanager/exceptions.py
+++ micromanager/exceptions.py
@@ -2,7 +2,15 @@ from googleapiclient.errors import HttpError
 
 
 def is_retryable_exception(err):
-    print(err)
+    """
+    Args:
+        err: An exception
 
+    Returns:
+        True if the exception should trigger a retry of certain operations
+
+    """
     if isinstance(err, HttpError):
         return err.resp.status in [409]
+
+    return False
